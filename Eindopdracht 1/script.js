9c732e84061175c2a1da29e11ee467a6a585b0c0
Omerfaruk Yildirim <omerfyy@hotmail.com>
2020-06-22 11:08:39 +0200

    first commit

---
+++ Eindopdracht 1/script.js
@@ -0,0 +1,20 @@
+
+let menuAan = function() {
+    let open = document.getElementById('menu');
+    open.classList.toggle('open');
+}
+let colorWhite = function() {
+    document.body.style.backgroundColor = 'white';
+}
+let colorRed = function() {
+    document.body.style.backgroundColor = 'red';
+}
+let colorOrange = function() {
+    document.body.style.backgroundColor = 'orange';
+}
+let colorYellow = function() {
+    document.body.style.backgroundColor = 'yellow';
+}
+let colorGreen = function() {
+    document.body.style.backgroundColor = 'green';
+}
